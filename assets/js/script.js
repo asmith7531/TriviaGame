e3aea8471acd5e1de854ca81b6591f7780f9dbd7
Austin Smith <austinsmith7531@gmail.com>
2019-03-22 00:01:14 -0400

    Needs to have the timers added and style but its actually functional now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,79 @@
-let questionGlobal = undefined;
-let rightGlobal = undefined;
-let wrong1Global = undefined;
-let wrong2Global = undefined;
-let wrong3Global = undefined;
+
 let answers = undefined;
+var correct = 0;
+var incorrect = 0;
 let i = 0;
+var keys = ["eea64161e14cdb72f109f54efd7c0faaaf6722588d40767af1033f7dcd194159","b97cd7530367f7ff9b7074e7e5eb2b203005cfc3cd924b5116ac1a536a7c0edc","98cf9e0421040c2fd6085b415d926d208cad97834f003e2faacd545e30e23315","5eb1904155f9e7c47010772ec9778d100d1983ab6c0e0eafa00ffca3ba92743c"]
+var random = Math.floor(Math.random()*keys.length)
+//rearranges arrays using some old dudes algorithm
 
 $.ajax ({
   method:"GET",
-  url: "https://opentdb.com/api.php?amount=50&token=3a7421f3cfb7125f2a34b00a5057da6c3604ebc3d30dcfb8442d6d611f5504af"
+  url: "https://opentdb.com/api.php?amount=50&token="+ keys[random]
 }).then( function (response) {
   console.log(response);
   console.log(i)
+  function randomShuffle(array){
+    for (var i=-1; i<array.length;i++){
+      var random = Math.floor(Math.random()*i+1);
+      array[i],array[random] = array[random],array[i];
+    }
+  }
+  $("#startBtn").on("click", function(event) {
+    begin();
+    event.preventDefault();
+  })
+
+  function iscorrect(){
+    alert("Correct!");
+    reset();
+    correct++
+    $(".correct").text("Correct Answers: " + correct);
+    getQuestion();
+  }
+  
+  function iswrong(){
+    alert("Incorrect!");
+   reset();
+    incorrect++
+    $(".incorrect").text("Incorrect Answers:" + incorrect);
+    getQuestion();
+  }
+
+  $(document).on("click", ".right", iscorrect)
+  $(document).on("click", ".wrong", iswrong)
+
+function getQuestion(){
+ 
   var questionAPI = response.results[i].question;
   var rightAPI = response.results[i].correct_answer;
   var wrong1API = response.results[i].incorrect_answers[0];
   var wrong2API = response.results[i].incorrect_answers[1];
   var wrong3API = response.results[i].incorrect_answers[2];
-  dataPull(questionAPI,rightAPI,wrong1API,wrong2API,wrong3API)
-  console.log(questionAPI)
-  $("#startBtn").on("click",begin());
-  }
-)
-
-//gets data from API to make available globally
-function dataPull(
-  question,
-  right,
-  wrong1,
-  wrong2,
-  wrong3) {
-    questionGlobal = question;
-    rightGlobal = right;
-    wrong1Global = wrong1;
-    wrong2Global = wrong2;
-    wrong3Global = wrong3;
-}
-
-
-
-function begin(){
-console.log(questionGlobal)
-  $('.startBtn').hide();
-  setTimeout(getQuestion(),5000);
-
-
-function getQuestion()  {
   i++
-  dataPull();
-}
-
 //creates the question and answers and adds them to the HTML, also calls the random shuffle function 
-function writeElements(questionGlobal,rightGlobal,wrong1Global, wrong2Global,wrong3Global)
-  {
-    console.log(questionGlobal)
-    $(".question").append($("<p>").text(questionGlobal).addClass("question"));
-    var answers = [rightGlobal, wrong1Global, wrong2Global, wrong3Global]
-    randomShuffle(answers);
-    //iterates through the answers array and adds the answers to the HTML
-    for (var i = -1; i < answers.length; i++) {
-    $(".answers").append($("<button>").addClass("answerBtn").text(answers[i]))  
-    }
+function writeElements()  {
+  
+  console.log(questionAPI)
+  $(".questions").append($("<p>").text(questionAPI).addClass("question"));
+  var answers = [rightAPI, wrong1API, wrong2API, wrong3API]
+  randomShuffle(answers);
+  //iterates through the answers array and adds the answers to the HTML
+  for (var i = 0; i < answers.length; i++) {
+    if(answers[i]===rightAPI){
+    $(".answers").append($("<button>").addClass("answerBtn right").text(answers[i]))  
   }
-
-writeElements()
-
-//rearranges arrays
-function randomShuffle(array){
-  for (var i=-1; i<array.length;i++){
-    var random = Math.floor(Math.random()*i+1);
-    array[i],array[random] = array[random],array[i];
+  else{
+    $(".answers").append($("<button>").addClass("answerBtn wrong").text(answers[i]))
   }
 }
-
-
 }
+setTimeout(writeElements(),5000);
+}
+  function begin()  {
+    $('#startBtn').remove();
+    getQuestion();
+  }
+})
+
+
